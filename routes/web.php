e059c5c0aacc33e9542b1ea5423a368f92c11d25
Sandi Anugrah <sandianugrah96@gmail.com>
2018-10-27 22:20:02 +0700

    menambah route baru

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::post('/post/pekerjaan', 'PostController@pekerjaan');
+Route::post('/post/pekerjaan', 'PostController@pekerjaan');//route baru
